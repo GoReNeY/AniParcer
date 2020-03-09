f6e2d99c50b800c81f4037abd723247bc65e9085
GoReNeY <57534011+GoReNeY@users.noreply.github.com>
2020-03-09 16:56:07 +0200

    Some cosmetic changes

--- connect.py
+++ connect.py
@@ -25,7 +25,8 @@ def export(connect):
         parced_table = parcer.parce(parcer.get_html(parcer.url))
 
         for hero_stats in parced_table:
-            sql_command = f"INSERT INTO {parcer.db} (heroname_as_dotaname,picks,wins,winrate,pickrate) VALUES ('{hero_stats['heroname_as_dotaname']}','{hero_stats['picks']}','{hero_stats['wins']}','{hero_stats['winrate']}','{hero_stats['pickrate']}')"
+            sql_command = f"INSERT INTO {parcer.db} (heroname_as_dotaname,picks,wins,winrate,pickrate) \
+                            VALUES ('{hero_stats['heroname_as_dotaname']}','{hero_stats['picks']}','{hero_stats['wins']}','{hero_stats['winrate']}','{hero_stats['pickrate']}')"
             cur.execute(sql_command)
 
 def update(connect):
@@ -33,10 +34,13 @@ def update(connect):
     parced_table = parcer.parce(parcer.get_html(parcer.url))
 
     for hero_stats in parced_table:
-        print(hero_stats)
-        sql_command = f"UPDATE {parcer.db} SET picks = {hero_stats['picks']}, wins = {hero_stats['wins']}, winrate = {hero_stats['winrate']}, pickrate = {hero_stats['pickrate']} WHERE heroname_as_dotaname = '{hero_stats['heroname_as_dotaname']}'"
+        sql_command = f"UPDATE {parcer.db} SET picks = {hero_stats['picks']}, wins = {hero_stats['wins']}, winrate = {hero_stats['winrate']}, pickrate = {hero_stats['pickrate']}\
+                        WHERE heroname_as_dotaname = '{hero_stats['heroname_as_dotaname']}'"
         cur.execute(sql_command)
         con.commit()
 
+def main():
+    update(con)
 
-update(con)
+if __name__ == "__main__":
+    main()
